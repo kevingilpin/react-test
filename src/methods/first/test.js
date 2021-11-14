da68f02e4477bbb09b343ff888326d45b8647268
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-11-14 14:27:44 -0800

    Create null test for first method

--- src/methods/first/test.js
+++ src/methods/first/test.js
@@ -1,6 +1,6 @@
-import React from "react";
-import $ from "../../";
-import "babel-polyfill";
+import React from 'react';
+import $ from '../../';
+import 'babel-polyfill';
 
 const $list = $(
   <ul>
@@ -10,12 +10,22 @@ const $list = $(
   </ul>
 );
 
-describe(".first()", () => {
-  it("returns the top-level tag by default", async () => {
-    expect($list.first().nodeName).toBe("UL");
+const $list2 = $(
+  <div>
+    <ul></ul>
+  </div>
+);
+
+describe('.first()', () => {
+  it('returns the top-level tag by default', async () => {
+    expect($list.first().nodeName).toBe('UL');
   });
 
-  it("returns the first item of a list", async () => {
-    expect($list.find("li").first().textContent).toEqual("A");
+  it('returns the first item of a list', async () => {
+    expect($list.find('li').first().textContent).toEqual('A');
+  });
+
+  it('returns null value when no items found', async () => {
+    expect($list2.find('li').first()).toEqual(null);
   });
 });
